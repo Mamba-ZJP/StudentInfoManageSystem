9956e411155304246297632934f165af5b3aac65
Mamba-ZJP <1330472951@qq.com>
2021-07-03 13:14:24 +0800

    完成user的添加学生信息。

--- src/com/whpu/servlet/AddStuServlet.java
+++ src/com/whpu/servlet/AddStuServlet.java
@@ -1,7 +1,9 @@
 package com.whpu.servlet;
 
+import com.alibaba.fastjson.JSON;
 import com.whpu.dao.StudentDao;
 import com.whpu.dao.StudentDaoImplement;
+import com.whpu.entity.Response;
 import com.whpu.entity.Student;
 
 import javax.servlet.ServletException;
@@ -18,7 +20,10 @@ public class AddStuServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doGet(req, resp);
         HttpServletRequest request = req;
+        HttpServletResponse response = resp;
+
         StudentDao studentDao = new StudentDaoImplement();
+        Response result = new Response();
 
         String sname = req.getParameter("sname");
         String sno = req.getParameter("sno");
@@ -32,6 +37,8 @@ public class AddStuServlet extends HttpServlet {
             studentDao.addStu(student);
         } catch (SQLException e) {
             e.printStackTrace();
+            result.setAll(404, null, "操作失败！");
+            response.getWriter().print(JSON.toJSONString(result));
         }
 
         resp.sendRedirect("selectAll");
